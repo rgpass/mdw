c7fc4fdf793178b737162f1889a11ddab969db58
Gerry Pass <rgpass@gmail.com>
2013-06-05 17:35:30 -0400

    Add SSL in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,8 @@ Mdw::Application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  # This has to be un-commented to force SSL in production.
+  config.force_ssl = true
 
   # See everything in the log (default is :info)
   # config.log_level = :debug
