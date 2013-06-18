14d332d84104aee392c6bccfe7b094e2ecefd86f
Gerry Pass <rgpass@gmail.com>
2013-06-18 09:26:34 -0400

    Add Google Apps meta tag

--- db/migrate/20130613220838_change_content_to_text.rb
+++ db/migrate/20130613220838_change_content_to_text.rb
@@ -1,3 +1,8 @@
+# Generated with:
+# $ rails generate migration change_content_to_text
+# This had to be done because a string by default on PostgreSQL
+# is limited to 255 characters. Text however has a much
+# larger limit. Now able to submit large stories.
 class ChangeContentToText < ActiveRecord::Migration
   def up
   	change_column :posts, :content, :text
